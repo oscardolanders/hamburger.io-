5f773f5f09874c880eb5f0bdbb8fd3fac707d543
oscardolanders <95285616+oscardolanders@users.noreply.github.com>
2023-03-10 11:45:28 +0000

    Add files via upload

---
+++ food1/javascript/test.js
@@ -0,0 +1,28 @@
+/*let tab = new Array(1,23,3);
+for( let i=0; i<tab.length;i++){ 
+    alert(tab[i]);
+} 
+*/
+/*function isPaindrome(str){
+  str =str.split('')
+  str = str.reverse()
+  return str.join('-')
+}
+
+ const  a = isPaindrome("aegrd")
+  
+ function isParcourir(a){
+    for(i=0;i<a.length;i++){
+        console.log(a[i])
+    }
+    return a[i]
+ }
+ console.log(a)
+   let b = prompt('veuillez rentrer une chaine de caractere');
+ let c = isParcourir(b)
+ console.log(c)
+ */
+//let a = prompt(" veuillez devinez ce nombre ");
+
+ 
+ 
